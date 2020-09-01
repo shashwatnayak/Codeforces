53b19b1c316360fc22cd80229143f5d7c585af31
Shashwat <shashwatnayak@outlook.com>
2020-09-02 00:25:37 +0530

    seg fault due to vector, need a fix

--- basics/Arrays_Vectors_Maps_Basics/cpp/sort.cc
+++ basics/Arrays_Vectors_Maps_Basics/cpp/sort.cc
@@ -17,7 +17,7 @@ void print(vi v){
 void bubble(vi& a){
   int n = a.size();
   for(int i = 0;i<n;i++){
-    for(int j = 0;j<n;j++){
+    for(int j = i + 1;j<n;j++){
       if(a[i] < a[j]){
         swap(a[i],a[j]);
       }
@@ -32,8 +32,58 @@ void insertion(vi& a){
 void selection(vi& a){}
 
 //mergesort o nlgn
-void mergesort(){
+//seg fault error in mergesort maybe due to vector, can try for array
 
+void merge(vi& a,int l,int m,int r){
+int i,j,k;
+
+int n1 = m - l + 1 ;
+int n2 = r - m ;
+vi left(n1);
+vi right(n2);
+for(i = 0 ;i<n1;i++){
+  left.push_back(a[l+i]);
+}
+for(j = 0;j<n2;j++){
+  right.push_back(a[k+1+j]);
+}
+i = 0;
+j = 0;
+k = l;
+
+while(i< n1 && j < n2 && l <= r){
+  if(left[i] <= right[j]){
+    a[k] = left[i];
+    i++;
+  }else{
+    a[k] = right[j];
+    j++;
+  }
+  k++;
+}
+while(i<n1){
+  a[k] = left[i];
+  i++;
+  k++;
+}
+while(j<n2){
+  a[k] = right[j];
+  j++;
+  k++;
+}
+
+}
+void mergesort(vi& a,int l ,int r){
+//split array into 2 parts
+//recursively follow up in first half
+//recursively follow up in second half;
+//merge both and return array
+if(l<r){
+int k = (l+r)/2;
+mergesort(a,l,k);
+mergesort(a,k+1,r);
+merge(a,l,k,r);
+}
 }
 //quicksort o lgn
 void quicksort(){
@@ -48,7 +98,10 @@ a.push_back(6);
 a.push_back(4);
 a.push_back(5);
 print(a);
-//sort(a.begin(),a.end());
-bubble(a);
-print(a);
+//sort(a.begin(),a.end());  // for algorithm header
+//qsort(a.begin(),a.end()) // for quicksort shortcut in algorithm header
+// bubble(a);
+// print(a);
+
+mergesort(a,0,a.size()-1);
 }
