f125d35bb1ae635268d274fb56bb1103a1a94340
Shashwat <shashwatnayak@outlook.com>
2020-10-06 23:48:27 +0530

    book exercise solutions reset

--- basics/book_exercises/2_2_1.cpp
+++ basics/book_exercises/2_2_1.cpp
@@ -7,32 +7,7 @@ typedef vector<int> vi;
 typedef map<int,int> mii;
 typedef pair<int,int> pii;
 
-bool duplicatenumber(vi a);
-mii m;
-for(int i = 0;i<a.size();i++){
-  m[a[i]]++;
-}
-map<int,int> iterator :: itr = m.begin();
-while(itr! = m.end()){
-  if(itr->second>1){
-    return true;
-  }
-  itr++;
-}
-return false;
-}
 
-pii findint(vi a,int v){
-  pii ans;
-  mii m;
-
-  for(int i = 0;i<a.size();i++){
-    m.insert(pii(a[i],v-a[i]));
-  }
-  mii iterator it = m.begin();
-  for(int i = )
-  
-}
 int main(){
 
 
