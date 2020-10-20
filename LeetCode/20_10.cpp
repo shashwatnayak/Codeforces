702d167d062c9f355ccea8a22e690424e187752b
Shashwat <shashwatnayak@outlook.com>
2020-10-20 23:34:47 +0530

    LeetCode 2 problems

---
+++ LeetCode/20_10.cpp
@@ -0,0 +1,111 @@
+ // All Possible Binary Tree
+ 
+ vector<TreeNode*> allPossibleFBT(int N) {
+      if(N==0) return {NULL};
+        if(N==1) return {new TreeNode(0)};
+        vector<TreeNode*> res;
+        for(int i = 1;i<=N-1;i+=2){
+            
+            if(i%2!=0 && ((N-i-1)&2!=0)){
+            vector<TreeNode*> left = allPossibleFBT(i);
+            vector<TreeNode*> right = allPossibleFBT(N-i-1);
+            
+            for(auto l : left){
+                for(auto r : right){
+                    TreeNode* root = new TreeNode(0);
+                    root->left = l;
+                    root->right = r;
+                    res.push_back(root);
+                }
+            }
+        }
+    }
+        return res;
+    }
+};
+
+// Mutable Segment Tree
+
+class NumArray {
+public:
+    int n;
+    vector<int>seg;
+    void build(int index,int low,int high,vector<int>nums)
+    {
+        if(low==high)
+        {
+            seg[index]=nums[low];
+            return;
+        }
+        int mid=(low+high)/2;
+        build(2*index+1,low,mid,nums);
+        build(2*index+2,mid+1,high,nums);
+        seg[index]=seg[2*index+1]+seg[2*index+2];
+        
+    }
+    
+    int query(int index,int low,int high,int l,int r)
+    {
+
+        if(low>=l && high<=r)  
+        {
+            return seg[index];
+        }
+        if(high<l || low >r) return 0;   
+
+        int mid=(low+high)/2;
+        int left=query(2*index+1,low,mid,l,r);
+        int right=query(2*index+2,mid+1,high,l,r);
+        return left+right;
+    }
+
+    
+    
+     void updatehelper(int ind,int low,int high,int pos,int val)
+     {
+        if(low==high)
+        {
+            seg[ind]=val;
+        }
+        else
+        {
+            int mid=(low+high)/2;
+            if(pos<=mid)
+            {
+                updatehelper(2*ind+1,low,mid,pos,val);
+            }
+            else
+            {
+                updatehelper(2*ind+2,mid+1,high,pos,val);
+            }
+            seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+         }
+     }
+    
+    NumArray(vector<int>& nums) {
+        n=nums.size();
+        if(n==0)
+            return ;
+        seg.resize(4*n);
+        
+        build(0,0,n-1,nums);
+        
+    }
+    
+    
+    void update(int i, int val) {
+        updatehelper(0,0,n-1,i,val);
+    }
+    
+    int sumRange(int i, int j) {
+        return query(0,0,n-1,i,j);
+        
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(i,val);
+ * int param_2 = obj->sumRange(i,j);
+ */
