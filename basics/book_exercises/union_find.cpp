37bafd0de4ae977227646d133a0b568625d8c7a1
Shashwat <shashwatnayak@outlook.com>
2020-11-04 23:48:43 +0530

    ufds updated with test case

--- basics/book_exercises/union_find.cpp
+++ basics/book_exercises/union_find.cpp
@@ -2,17 +2,44 @@
 #include<vector>
 using namespace std;
 typedef vector<int> vi;
-class UnionFind { // OOP style
-private: vi p, rank; // remember: vi is vector<int>
+
+class UnionFind{
+private:
+vi p,rank;
 public:
-UnionFind(int N) { rank.assign(N, 0);
-p.assign(N, 0); for (int i = 0; i < N; i++) p[i] = i; }
-int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
-bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-void unionSet(int i, int j) {
-if (!isSameSet(i, j)) { // if from different set
-int x = findSet(i), y = findSet(j);
-if (rank[x] > rank[y]) p[y] = x; // rank keeps the tree short
-else { p[x] = y;
-if (rank[x] == rank[y]) rank[y]++; }
-} } };
+UnionFind(int n){
+    rank.assign(n,0);
+    p.assign(n,0);
+    for(int i = 0;i<n;i++){
+        p[i] = i;
+    }
+}
+int findSet(int i){
+return (p[i]==i) ? i : (p[i] = findSet(p[i]));
+}
+bool isSameSet(int i,int j){
+return (findSet(i)==findSet(j));
+}
+void UnionSet(int i,int j){
+if(!isSameSet(i,j)){
+int x = findSet(i);
+int y = findSet(j);
+if(rank[x]>rank[y]){
+    p[y] = x;
+}else{
+    p[x] = y;
+    if(rank[x]==rank[y])rank[y]++;
+}
+}}};
+
+int main(){
+    UnionFind uf(4);
+    uf.UnionSet(0,1);
+    uf.UnionSet(2,3);
+    uf.UnionSet(4,3);
+    //uf.UnionSet();
+    //uf.UnionSet();
+
+    
+
+}
