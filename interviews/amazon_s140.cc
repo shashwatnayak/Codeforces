b9f07284e0023c8cd109785e109026b204821d4c
Shashwat <shashwatnayak@outlook.com>
2020-11-23 22:57:54 +0530

    amazon interview question for 2 set added. TBD

---
+++ interviews/amazon_s140.cc
@@ -0,0 +1,19 @@
+/* 1.Given an integer array and a constant number X, print all pair of number in the array whose product is equal to X.
+follow ups: how will you do in O(n)? how will you handle duplicate pairs?
+2.He asked do I know level order traversal of binary tree. He ask me to code then, he changed the question and asked about spiral order traversal of binary Tree and asked me to code it.
+3.In a BST two nodes were swapped. Given the pointer to root node find the two nodes and rectify the tree. He asked the approach then asked me to code the same in collabedit.
+4.Given an integer array, find and print three element in the array whose product is maximum. Code was required.
+5.Given a sorted array of n integers, count and display number of triplets such that a[i] < a[j] < a[k] 0(n) time. Code was required.
+6.In a given string some of the characters are replaced by question mark, and you can replace question mark with any character. Given such a string find total number of palindrome that can created. String contains only [a-z] characters and question marks can also be only replaced by [a-z]. Example: Input String: String str=”a??a” Output: 26<F10>
+7.Given a binary tree all the leaf nodes in the form of a doubly linked list. Find the height of the tree.
+8.Given N sorted LinkList of different length, merge them into a single sorted link list. Pseudo code was required.
+9.Given a number d and size of array N. Print all combination of element in the array such that first element of array is d and next element in the array can be +1 or -1 the previous element in the array. Code was required.
+*/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+
+return 0;
+}
---
+++ interviews/amazon_s77.cc
@@ -0,0 +1,19 @@
+/*
+ * 1.Given an of increasing and decreasing elements, find max element 
+ * 2.Given an array of unsorted elements, find the minimum difference between any 2 elements in the array.
+ * 3.Given an array of positive numbers, find the maximum sum of a subsequence with the constraint that no 2 numbers in the sequence should be adjacent in the array. (extended to negative numbers)
+ * 4.Write a function to print the level order traversal of a binary tree in spiral form.
+ * 5.Write a function to find the next smallest palindrome number of the given number. for example: if given number is 12345, then the next smallest palindrome is 12421.
+ * 6.Given 2 strings str1 and str2. What is the efficient way to navigate from str1 to str2? The constraints are i) a string can be changed to another string by changing only one character. ii) all the intermediate strings must be present in dictionary. If not possible, return “not possible to navigate from str1 to str2”. (pre-processing is allowed and enough memory is available). for example: str1 = feel and str2 = pelt, then the navigation is feel -> fell -> felt -> pelt (Hint: Graph)
+ * 7.Given two numbers represented by two linked lists, write a function that returns sum list. The sum list is linked list representation of addition of two input numbers. It is not allowed to modify the lists. Also, not allowed to use explicit extra space.
+ * 8.Given a matrix of characters and a string, find whether the string can be obtained from the matrix. From each character in the matrix, we can move up/down/right/left. for example, if the matrix[3][4] is and the string is follow, then the function should return true.
+ * 9.Write a program to check whether the given binary tree is BST.
+ * 10.Given an input string, write a function that returns the compressed string for the input string in INPLACE. (no extra memory) (length of compressed string < = length of input string) For example, if the input string is “aaabcdeeee”, then the function should return “a3b1c1d1e4?.
+ * */
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+return 0;
+}
