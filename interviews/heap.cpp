e362cb9802d86c499131d60a29ce5cc6e1c5fee1
Shashwat <shashwatnayak@outlook.com>
2020-12-13 09:57:29 +0530

    7 problems done, not tested

--- interviews/heap.cpp
+++ interviews/heap.cpp
@@ -1,3 +1,122 @@
 #include<iostream>
 using namespace std;
 
+
+class Heap {
+    vector<int> v;
+    int sze;
+
+    int parent(int i) { return i / 2;}
+    int left(int i) { return 2 * i; }
+    int right(int i) { return 2 * i + 1; }
+
+    void heapify(int i){
+
+        // left child comparison
+        int posOfLargest = i;
+        if (left(i) <= sze && v[left(i)] > v[i]){
+            posOfLargest = left(i);
+        }
+
+        if (right(i) <= sze && v[right(i)] > v[posOfLargest]){
+            posOfLargest = right(i);
+        }
+
+        if (i != posOfLargest){
+            // I HAVE to swap
+            swap(v[i], v[posOfLargest]);
+            heapify(posOfLargest);
+        }
+    }
+
+public:
+    Heap() {
+        sze = 0;
+        v.push_back(-1);    // empty position
+    }
+
+    int top() {
+        if (empty()) return -1;
+        return v[1];
+    }
+
+    bool empty() {
+        return sze == 0;
+    }
+
+    void pop() {
+        if (empty()) return;
+
+        swap(v[1], v[sze]);
+        v.pop_back();
+        --sze;
+
+        heapify(1);
+    }
+
+    void push(int d) {
+        // push and compare with the parent
+        v.push_back(d);
+        ++sze;
+
+        int i = v.size() - 1;
+        while(i > 1 && v[i] > v[parent(i)]){
+            swap(v[i], v[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+    void print(){
+        for(int i = 1; i <= sze; ++i){
+            cout << v[i] << " ";
+        }
+        cout << endl;
+    }
+};
+void heapify(int arr[],int n,int i){
+
+int largest = i;
+int l = 2*i + 1;
+int r = 2*i + 2;
+
+if(l<n && arr[l] > arr[largest]){
+    largest = l;
+}
+
+if(r<n && arr[r] > arr[largest]){
+    largest = r;
+}
+if(largest != i){
+    swap(arr[i],arr[largest]);
+    heapify(arr,n,largest);
+}
+}
+
+// Max heap
+void kthLargest(int arr[],int n){
+    // Build Heap
+    for(int i = n/2 - 1;i>=0;i--){
+        heapify(arr,n,i);
+    }
+
+    while(k!=0){
+        // Extract Min/Max k times
+        heapify();
+    }
+}
+void heapsort(){
+    // Build Heap
+    for(int i = n/2 -1;i>=0 ;i--){
+        heapify(arr,n,i);
+    }
+    // Extract one element from heap and move to end;
+    // Max Heap - ascending order;
+    for(int i = n-1;i>=0;i--){
+        swap(arr[0],arr[i]);
+        // Call heapify on reduced heap 
+        heapify(arr,i,0);
+    }
+}
+int main(){
+
+}
--- interviews/stack_queue.cc
+++ interviews/stack_queue.cc
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stack>
+#include<queue>
 using namespace std;
 
 class Stack{
@@ -114,3 +116,56 @@ while(front!=NULL){
 }
 }
 };
+
+void reverse_Str(string a){
+    stack<char> s;
+    for(int i = 0;i<a.size();i++){
+        s.push(a[i]);
+    }
+    for(int i = 0;i<a.size();i++){
+        a[i] = s.top();
+        s.pop();
+    }
+}
+
+stack<char> st;
+string s;
+void insert_at_bottom(char x){
+    if(st.size()==0){
+        st.push(x);
+    }else{
+        char a = st.top();
+        st.pop();
+        insert_at_bottom(x);
+        st.push(a);
+    }
+}
+void stack_reverse(){
+while(st.size()>0){
+    char x = st.top();
+    st.pop();
+    stack_reverse();
+    insert_at_bottom(x);
+}
+}
+void insert_sortstack(stack<int> s,int x){
+if(s.size()==0 || x > s.top()){
+s.push(x);
+return;
+}
+int tmp = s.top();
+s.pop();
+insert_sortstack(s,x); 
+s.push(tmp);
+}
+void sortStack(stack<int> s){
+if(s.size()!=0){
+int x = s.top();
+s.pop();
+sortStack(s);
+insert_sortstack(s,x);
+}
+}
+int main(){
+
+}
