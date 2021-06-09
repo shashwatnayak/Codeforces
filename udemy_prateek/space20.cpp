0431004dc881f3eb546e970775a80baaed287034
Shashwat Nayak <shashwatnayak@outlook.com>
2021-06-09 20:22:50 +0530

    updated all new files

--- udemy_prateek/space20.cpp
+++ udemy_prateek/space20.cpp
@@ -3,6 +3,31 @@
 
 using namespace std;
 
+void replace(char *str){
+// Calculate spaces
+int spaces = 0;
+for(int i=0;str[i]!='\0';i++){
+    if(str[i]== ' '){
+    spaces++;
+    }
+}
+// Shift the string right/modify to accomodate the characaters in single pass
+int idx = strlen(str) + 2*spaces;
+str[idx] = '\0';
+for(int i=strlen(str)-1;i>=0;i--){
+    if(str[i]==' '){
+        str[i-1] = '0';
+        str[i-2] = '2';
+        str[i-3] = '%';
+    }else{
+        str[i-1] = str[i];
+    }
+}
+}
 int main(){
-    
+char input[10000];
+getline(cin,input);
+replace(input);
+cout << input;
+return 0;    
 }
